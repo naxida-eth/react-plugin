892ed3cfb8ca4cff41a780a92015bf741face2af
chengkun <326193664@qq.com>
2022-04-29 21:11:12 +0800

    coding react-popper-component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import PMedia from "./components/PMedia";
 import Skeleton from './components/Skeleton'
 import photo from './images/photo.jpg'
 
+import Popper from "./components/Popper";
+
 function App() {
   return (
     <>
       {/* <PMedia></PMedia> */}
-      <Skeleton src={photo} style={{ height: 460, width: 460 }}>
+      {/* <Skeleton src={photo} style={{ height: 460, width: 460 }}>
         <img src={photo} alt="" />
-      </Skeleton>
+      </Skeleton> */}
+      <Popper></Popper>
     </>
   );
 }
